aef669621eae1c9ac203db7c2fbfc12cb405c71b
thefantom67 <cdf.malvy.bruno@gmail.com>
2019-09-29 15:36:04 +0000

    feat(main): Regex ok

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
- $('#email').val().match(emailRegex);
-
-//match email address
-var emailRegex = '^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$'; 
-//match username
-var usernameRegex = '/^[a-z0-9_-]{3,16}$/'; 
-//match password
-var passwordRegex = '/^[a-z0-9_-]{6,18}$/'; 
-//Match 8 to 15 character string with at least one upper case letter, one lower case letter, and one digit (useful for passwords).
-var passwordStrengthRegex = /((?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,15})/gm; 
-//match elements that could contain a phone number
-var phoneNumber = /[0-9-()+]{3,20}/; 
+$(document).ready(function(){  
+    $("#monFormulaire").submit(function(e) {
+        var email = $('#email').val();
+        var password = $('#password').val();
+        var cp = $('#cp').val();
+        var tel = $('#tel').val();
+        
+        if(!email.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)){
+			alert("Entrez un e-mail valide !");
+		}  
+		
+		  if(!password.match(/^.{6,32}$/)){
+			alert("Le mot de passe doit être compris entre 6-32 charactères");
+		} 
+		
+		if(!password.match(/(?=.*\d)(?=.*[A-Z])(?=.*\W)./)){
+			alert("Le mot de passe doit contenir au moins 1 majuscule, 1 chiffe et 1 charactère spécial");
+		}
+		if(!cp.match(/(\d{2}[ ]?\d{3})/)){
+		    alert("Mauvais code postal");
+		}
+		if(!tel.match(/[0-9]{10}/)){
+		    alert("Numéro de téléphone invalide");
+		}
+		
+    });
+});
